c0ffe693d7e12c6e2ae7ef5a78260011f023b102
GimiOne <dim.zaikin2013@gmail.com>
2019-04-15 12:03:39 +0300

    fix ori and acc folder

--- ori/script.js
+++ ori/script.js
@@ -1,20 +1,16 @@
-let absol = document.querySelector('absol');
-let alf = document.querySelector('alf');
-let bet = document.querySelector('bet');
-let gamm = document.querySelector('gamm');
+let absol = document.querySelector('#absol');
+let alf = document.querySelector('#alf');
+let bet = document.querySelector('#bet');
+let gamm = document.querySelector('#gamm');
 
 var x = 0, y = 0,
     vx = 0, vy = 0,
 	ax = 0, ay = 0;
-	
 
- 
-if (window.DeviceMotionEvent != undefined) {
-	window.ondevicemotion = function(e) {
-		alf.innerText = event.accelerationIncludingGravity.x;
-		bet.innerText = event.accelerationIncludingGravity.y;
-		gamm.innerText = event.accelerationIncludingGravity.z;
-	}
- 
-	
+
+window.ondevicemotion = function(e) {
+	console.log(1);
+	alf.innerText = event.accelerationIncludingGravity.x;
+	bet.innerText = event.accelerationIncludingGravity.y;
+	gamm.innerText = event.accelerationIncludingGravity.z;
 }
